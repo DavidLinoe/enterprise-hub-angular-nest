86b4608fbe16e42efd9c29f0097346bd7ea34dad
DavidLinoe <davideduardolino@outlook.com>
2026-05-23 23:43:12 -0300

    feat: criando variveis de estilo padrao

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./layout/navbar/containers/navbar.component";
-import { NavigationComponent } from "./layout/navigation/containers/navigation.component";
-import { SidebarComponent } from "./layout/sidebar/containers/sidebar.component";
+import { NavbarComponent } from './layout/navbar/containers/navbar.component';
+import { NavigationComponent } from './layout/navigation/containers/navigation.component';
+import { SidebarComponent } from './layout/sidebar/containers/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, NavigationComponent, SidebarComponent],
+  imports: [NavbarComponent, NavigationComponent, SidebarComponent],
   templateUrl: './app.html',
 })
 export class App {
