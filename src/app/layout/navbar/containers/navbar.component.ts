b184f99a215b6ad5cf287a52e2842f0500271a03
DavidLinoe <davideduardolino@outlook.com>
2026-05-23 23:43:41 -0300

    feat: criando componente de navegacao

--- src/app/layout/navbar/containers/navbar.component.ts
+++ src/app/layout/navbar/containers/navbar.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal, Signal } from '@angular/core';
+import { NAVBAR_ITEMS, NavbarItem } from '../models/navbar.model';
+import { Router } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [CommonModule],
   selector: 'layout-navbar',
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(public router: Router) {}
+
+  public navbarItems: Signal<NavbarItem[]> = signal(NAVBAR_ITEMS);
+}
